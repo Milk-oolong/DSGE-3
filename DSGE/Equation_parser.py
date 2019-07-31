c497a72f2b2354c2da0e5c9b849ec803c4db818f
lesibius <clementin.castellano@gmail.com>
2019-07-31 18:41:19 +0200

    Parser/lexer functional. Next step: create the linked list of computations

--- DSGE/Equation_parser.py
+++ DSGE/Equation_parser.py
@@ -8,6 +8,23 @@ FUNCTIONS = {
     }
 
 
+LAMBDA_BINOP = {
+    '+': lambda x, y : x + y,
+    '-': lambda x, y : x - y,
+    '/': lambda x, y : x - y,
+    '*': lambda x, y : x * y,
+    '**': lambda x, y : x ** y
+    }
+
+def get_dependencies(tree,acc = []):
+    if tree[0] is None:
+        if type(tree[1]) == str:
+            acc.append(tree[1])
+    else:
+        for t in tree[1:]:
+            get_dependencies(t,acc)
+    return acc
+
 class Parser:
     """
     Base class for a lexer/parser that has the rules defined as methods
@@ -34,6 +51,9 @@ class Calc(Parser):
         'LPAREN', 'RPAREN', 'COMMA'
     )
 
+    variables = {}
+    end_of_chain_variables = set()
+
     # Tokens
 
     t_PLUS = r'\+'
@@ -46,6 +66,9 @@ class Calc(Parser):
     t_RPAREN = r'\)'
     t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
     t_COMMA = r','
+
+    def get_parameters(self):
+        return {p for p,f in self.variables.items() if f is None}
 
     def t_FLOAT(self, t):
         '(\d*[.])?\d+'
@@ -77,52 +100,36 @@ class Calc(Parser):
         ('right', 'UMINUS'),
     )
 
-    def p_statement_assign(self, p):
-        'statement : NAME EQUALS expression'
-        print(p[1])
-        self.variables[p[1]] = p[3]
-
-    def p_statement_expr(self, p):
-        'statement : expression'
-        print(p[1])
-
-    def p_expression_binop(self, p):
-        """
-        expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression
-                  | expression EXP expression
-        """
-        # print [repr(p[i]) for i in range(0,4)]
-        if p[2] == '+':
-            p[0] = p[1] + p[3]
-        elif p[2] == '-':
-            p[0] = p[1] - p[3]
-        elif p[2] == '*':
-            p[0] = p[1] * p[3]
-        elif p[2] == '/':
-            p[0] = p[1] / p[3]
-        elif p[2] == '**':
-            p[0] = p[1] ** p[3]
-
-    def p_expression_function(self,p):
-        'statement : function'
-        fun = FUNCTIONS[p[1][0]]
-        params = p[1][1:]
-        print(fun,params)
+    def p_statement(self,p):
+        """statement : NAME EQUALS atom"""
+        #Get dependencies
+        dependencies = get_dependencies(p[3])
+        
+        self.variables[p[1]] = {
+            'function': p[3],
+            'dependencies': dependencies
+        }
+        self.end_of_chain_variables.add(p[1])
+        for d in dependencies:
+            if d not in self.variables.keys():
+                self.variables[d] = None
+            elif d in self.end_of_chain_variables:
+                self.end_of_chain_variables.remove(d)
+        
 
     def p_arglist(self,p):
-        """arglist : arglist COMMA NAME
-                   | arglist COMMA FLOAT
-                   | FLOAT
-                   | NAME"""
+        """
+        arglist : arglist COMMA NAME
+                   | arglist COMMA number
+                   | number
+                   | NAME
+        """
         if len(p) == 4:
             _ = p[1].copy()
-            _.append(p[3])
+            _.append([None,p[3]])
             p[0] = _
         else:
-            p[0] = [p[1]]
+            p[0] = [[None,p[1]]]
 
     def p_parameters(self,p):
         """parameters : LPAREN RPAREN 
@@ -134,34 +141,81 @@ class Calc(Parser):
 
     def p_function(self, p):
         'function : NAME parameters'
-        p[0] = [p[1]] + p[2]
+        fun = FUNCTIONS[p[1]]
+        p[0] = [fun] + p[2]
+
+    def p_number(self, p):
+        """number : INTEGER 
+                      | FLOAT"""
+        p[0] = p[1]
+
+    def p_number_binop(self,p):
+        """
+        number : number PLUS number
+               | number MINUS number
+               | number TIMES number
+               | number DIVIDE number
+               | number EXP number
+        """
+        if p[2] == '+':
+            p[0] = p[1] + p[3]
+        elif p[2] == '-':
+            p[0] = p[1] - p[3]
+        elif p[2] == '*':
+            p[0] = p[1] * p[3]
+        elif p[2] == '/':
+            p[0] = p[1] / p[3]
+        elif p[2] == '**':
+            p[0] = p[1] ** p[3]
+
+    def p_atom_binop(self,p):
+        """
+        atom : atom PLUS atom
+             | atom MINUS atom
+             | atom TIMES atom
+             | atom DIVIDE atom
+             | atom EXP atom
+        """
+        binop = LAMBDA_BINOP[p[2]]
+        p[0] = [lambda x,y: binop(x,y)] + [p[1],p[3]]
+
+    def p_atom_number_binop_atom(self,p):
+        """atom : number PLUS atom
+                | number MINUS atom
+                | number TIMES atom
+                | number DIVIDE atom
+                | number EXP atom"""
+        binop = LAMBDA_BINOP[p[2]]
+        p[0] = [lambda x,y: binop(x,y)] + [[None,p[1]],p[3]]
 
 
-    def p_expression_uminus(self, p):
-        'expression : MINUS expression %prec UMINUS'
+    def p_number_uminus(self, p):
+        'number : MINUS number %prec UMINUS'
         p[0] = -p[2]
-        
-    def p_expression_group(self, p):
-        'expression : LPAREN expression RPAREN'
+
+    def p_number_group(self,p):
+        'number : LPAREN number RPAREN'
         p[0] = p[2]
 
-    def p_expression_INTEGER(self, p):
-        'expression : INTEGER'
+    def p_atom_group(self,p):
+        'atom : LPAREN atom RPAREN'
+        p[0] = p[2]
+
+    def p_variable(self,p):
+        'variable : NAME'
         p[0] = p[1]
 
+    def p_atom_number(self,p):
+        """atom : number"""
+        p[0] = [None,p[1]]
 
-    def p_expression_FLOAT(self, p):
-        'expression : FLOAT'
+    def p_atom_variable(self,p):
+        """atom : variable"""
+        p[0] = [None, p[1]]
+
+    def p_atom_function(self,p):
+        """atom : function"""
         p[0] = p[1]
-        
-    def p_expression_name(self, p):
-        'expression : NAME'
-        try:
-            p[0] = self.names[p[1]]
-        except LookupError:
-            print("Undefined name '%s'" % p[1])
-            p[0] = 0
-
 
     def p_error(self, p):
         if p:
