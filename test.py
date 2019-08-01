a348efeffac70aac5a685a0be8b12e5ac1aab46d
lesibius <clementin.castellano@gmail.com>
2019-08-01 18:11:05 +0200

    Modification of test file to store file

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ from DSGE.Econ_model import Econ_model
 from DSGE.Equation_parser import Econ_model_parser, get_dependencies
 from DSGE.Computation import make_equations, evaluate_function_tree
 
-model = Econ_model('IMF',join(getcwd(),'simple_model'),join(getcwd(),'params'))
+model = Econ_model('IMF',join(getcwd(),'models','test','simple_model'),join(getcwd(),'models','test','params'))
 model(10,4)
 
-print(model.results['K'][0])
+print(model.results['F'][0])
 
 
 
