d85633ab1eaf012e120fa0f5013b609bff7db873
lesibius <clementin.castellano@gmail.com>
2019-08-01 13:58:28 +0200

    Cleaning folder

--- test.py
+++ test.py
@@ -5,4 +5,10 @@ from DSGE.Equation_parser import Econ_model_parser, get_dependencies
 from DSGE.Computation import make_equations, evaluate_function_tree
 
 model = Econ_model('IMF',join(getcwd(),'simple_model'),join(getcwd(),'params'))
-model(10,20)
+model(10,4)
+
+print(model.results['K'][0])
+
+
+
+
