8801777bfd833ce6874175473cc434f0e0c44642
lesibius <clementin.castellano@gmail.com>
2019-08-02 12:50:51 +0200

    Test for v0.2

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ from DSGE.Equation_parser import Econ_model_parser, get_dependencies
 from DSGE.Computation import make_equations, evaluate_function_tree
 
 model = Econ_model('IMF',join(getcwd(),'models','test','simple_model'),join(getcwd(),'models','test','params'))
-model(1,100)
+model(1,10)
 
-print(model.results['Y'][0])
-#print(model.results)
+res = model.results['x'][0]
+#print(res)
+print(model.results)
 
 from DSGE.Computation import Variable, Lagged_variable
 
